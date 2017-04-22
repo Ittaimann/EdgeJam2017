a687b199bb489cc5c794317532306f6b55f3f6b1
Ittaimann <ittaiam@gmail.com>
2017-04-21 23:11:49 -0700

    kill player changed to on collision

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KillPlayer : MonoBehaviour {
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
