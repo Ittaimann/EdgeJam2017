f072430f48a1e298b7051a43e90fb2934c53c81f
Roldan Gammad <rgammad@uci.edu>
2017-04-21 20:52:13 -0700

    Added player movement, health, death, and screen shake scripts

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+public class Death : MonoBehaviour {
+    Health health;
+    SpriteRenderer sprite;
+    bool spriteDisable = false;
+
+    void Start()
+    {
+        health = GetComponent<Health>();
+        sprite = transform.root.GetComponentInChildren<SpriteRenderer>();
+        health.onDeath += Health_onDeath;
+        health.onDamage += Health_onDamage;
+    }
+
+    private void Health_onDamage(float amount)
+    {
+        Debug.Log("Health: " + health.healthPercent);
+        StartCoroutine(_DamageFlash());
+    }
+    private IEnumerator _DamageFlash()
+    {
+        spriteDisable = true;
+        sprite.enabled = false;
+        yield return new WaitForSeconds(.05f);
+        sprite.enabled = true;
+    }
+
+    private void Health_onDeath()
+    {
+        //screenshake
+        Debug.Log(transform.name + " has died");
+        Destroy(transform.root.gameObject);
+        health.onDeath -= Health_onDeath;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public float minJumpVelocity = 2.5f;
+    public float maxJumpVelocity = 5.0f;
+    
+    public int maxJumpCount = 2;
+    
+    private Rigidbody2D rigid;
+    private bool jump = false;
+    private bool jumpCancel = false;
+    private bool isGrounded = false;
+    private int jumpCount = 2;
+    [HideInInspector]
+    public static SpriteRenderer sprite;
+
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        jumpCount = maxJumpCount;
+        sprite = GetComponentInChildren<SpriteRenderer>();
+	}
+	
+	void Update () {
+        _MoveInput();
+        _JumpInputCheck();
+    }
+    void FixedUpdate()
+    {
+        _Jump();
+    }
+
+    private void _MoveInput()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        sprite.flipX = _FlipFace(horizontal);
+        rigid.velocity = new Vector2(horizontal * speed, rigid.velocity.y);
+    }
+    private bool _FlipFace(float horizontal)
+    {
+        if (horizontal > 0)
+            return false;
+        if (horizontal == 0)
+            return sprite.flipX;
+        return true;
+    }
+    private void _JumpInputCheck()
+    {
+        if (Input.GetButtonDown("Jump") && !jump)
+            jump = true;
+        if (Input.GetButtonUp("Jump") && !_isGrounded())
+            jumpCancel = true;
+    }
+    private void _Jump()
+    {
+        if (jump)
+        {
+            if (jumpCount > 0)
+            {
+                rigid.velocity = new Vector2(rigid.velocity.x, 0);
+                rigid.velocity += new Vector2(0, maxJumpVelocity);
+                jumpCount--;
+            }
+            if (_isGrounded())
+                jump = false;
+        }
+        else
+            jumpCount = maxJumpCount;
+        if (jumpCancel)
+        {
+            if(rigid.velocity.y > minJumpVelocity)
+            {
+                rigid.velocity = new Vector2(rigid.velocity.x, 0);
+                rigid.velocity += new Vector2(0, minJumpVelocity);
+            }
+            jumpCancel = false;
+        }
+    }
+    private bool _isGrounded()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - .55f), Vector2.down);
+        if (!ray)
+            return false;
+        if (ray.collider.CompareTag("Ground") && ray.distance <= 0)
+            return true;
+        return false;
+    }
+    
+}
