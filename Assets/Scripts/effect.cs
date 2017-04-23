669d3b65d8ae1822799c8405af4db6fbd7361915
Ittaimann <ittaiam@gmail.com>
2017-04-22 18:50:29 -0700

    SHADERS

---
+++ Assets/Scripts/effect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class effect : MonoBehaviour {
+
+    public Material material;
+    // Use this for initialization
+
+ 
+
+    // Update is called once per frame
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+       
+        Graphics.Blit(source, destination,material);
+        return;
+        
+
+       
+    }
+}
