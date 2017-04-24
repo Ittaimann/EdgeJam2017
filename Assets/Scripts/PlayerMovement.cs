09a46c924fc2410bb3998e9119690942f5d8b490
Roldan Gammad <rgammad@uci.edu>
2017-04-23 22:30:35 -0700

    fixed player prefab, main menu transition, door placement, and fixed player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,20 +12,18 @@ public class PlayerMovement : MonoBehaviour
     public int maxJumpCount = 2;
 
     private bool isDashing;
-    private bool finishedDashing;
-    public bool isInvincible;
     public float dashSpeed;
+    public float dashDistance;
 
     private Rigidbody2D rigid;
     private bool jumpPressed = false;
     private bool jumpCancel = false;
     [SerializeField]
-    private bool isGrounded = false;
     private bool isMoving = false;
     private int jumpCount = 2;
     [HideInInspector]
     public SpriteRenderer sprite;
-    private AttackController ac;
+    private Animator anim;
 
     public AudioClip jumpSound;
     public AudioClip dashSound;
@@ -35,23 +33,17 @@ public class PlayerMovement : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         jumpCount = maxJumpCount;
         sprite = GetComponentInChildren<SpriteRenderer>();
-        ac = GetComponentInChildren<AttackController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        _DashHelper();
         _MoveInput();
         _JumpInputCheck();
         _DashInputCheck();
+        _UpdateAnimations();
     }
 
-    void _DashHelper()
-    {
-        isDashing = ac.isDashing;
-
-        finishedDashing = ac.finishedDashing;
-    }
 
     void FixedUpdate()
     {
@@ -59,6 +51,13 @@ public class PlayerMovement : MonoBehaviour
             jumpCount = maxJumpCount;
         _Jump();
         _Dash();
+    }
+
+    private void _UpdateAnimations()
+    {
+        anim.SetBool("IsMoving", _isMoving());
+        anim.SetBool("IsGrounded", _isGrounded());
+        anim.SetBool("IsDashing", IsDashing());
     }
 
     private void _MoveInput()
@@ -139,12 +138,6 @@ public class PlayerMovement : MonoBehaviour
         return isMoving;
     }
 
-
-    public bool IsInvincible()
-    {
-        return isInvincible;
-    }
-
     public bool IsDashing()
     {
         return isDashing;
@@ -152,34 +145,17 @@ public class PlayerMovement : MonoBehaviour
 
     public void _Dash()
     {
-        if (isDashing && !finishedDashing)
-        {
-            if (sprite.flipX)
-                rigid.velocity = new Vector2(dashSpeed, 0);
-            else
-                rigid.velocity = new Vector2(dashSpeed * -1, 0);
-
-
-        }
-
-        else if (isDashing && finishedDashing)
-            FinishedDashMovement();
+        if (isDashing)
+            StartCoroutine(_DashRoutine());
     }
-
-
-    public void FinishedDashMovement()
+    private IEnumerator _DashRoutine()
     {
-        rigid.velocity = new Vector2(0, rigid.velocity.y);
-        finishedDashing = true;
-        isInvincible = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.tag == "MovingPlatform")
-        {
-            transform.parent = other.transform;
-        }
+        if (sprite.flipX)
+            transform.position = Vector2.Lerp(transform.position, (Vector2)transform.position + new Vector2(dashDistance, 0), dashDistance / dashSpeed);
+        else
+            transform.position = Vector2.Lerp(transform.position, (Vector2)transform.position - new Vector2(dashDistance, 0), dashDistance / dashSpeed);
+        yield return new WaitForSeconds(dashDistance / dashSpeed);
+        isDashing = false;
     }
 
     private void OnCollisionExit2D(Collision2D other)
