4bb5ff0cdcfb64c2a1543bc1db947fdacd7f493e
andresg6 <andresg6@uci.edu>
2017-04-23 20:16:50 -0700

    Scene Transistion shader; 50% done with general mid-transition scene

---
+++ Assets/Scripts/SceneManagement/SceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public void swapToScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void swapToSceneDelayed(string scene, float time)
+    {
+        StartCoroutine(ExecuteSceneSwap(scene, time));
+    }
+
+    private IEnumerator ExecuteSceneSwap (string scene, float time)
+    {
+        yield return new WaitForSeconds(time);
+        swapToScene(scene);
+    }
+}
---
+++ Assets/Scripts/UI/FontFader.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FontFader : MonoBehaviour {
+    public enum FadeState
+    {
+        FadeIn,
+        Rest,
+        FadeOut,
+        Blank
+    }
+    public float fadeTime = 1;
+    public float restTime = 2.5f;
+    public SceneController sc;
+    public string targetScene;
+
+    private FadeState fadeState;
+    private Text fadeText;
+    private float timeLeft;
+
+	void Start () {
+        fadeText = this.GetComponent<Text>();
+        fadeState = FadeState.FadeIn;
+        timeLeft = fadeTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeLeft -= Time.deltaTime;
+
+		switch(fadeState)
+        {
+            case (FadeState.FadeIn):
+
+                changeAlpha(1-(timeLeft / fadeTime));
+                if(timeLeft <= 0)
+                {
+                    fadeState = FadeState.Rest;
+                    timeLeft = restTime;
+                }
+                break;
+
+            case (FadeState.Rest):
+    
+                if(timeLeft <= 0)
+                {
+                    fadeState = FadeState.FadeOut;
+                    timeLeft = fadeTime;
+                }
+                break;
+
+            case (FadeState.FadeOut):
+
+                changeAlpha(timeLeft / fadeTime);
+                if(timeLeft <= 0)
+                {
+                    fadeState = FadeState.Blank;
+                    sc.swapToScene(targetScene);
+                }
+                break;
+        }
+	}
+
+    private void changeAlpha(float alpha)
+    {
+        Color c = fadeText.color;
+        fadeText.color = new Color(c.r, c.g, c.b, alpha);
+    }
+}
